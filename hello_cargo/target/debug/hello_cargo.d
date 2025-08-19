1dc9c2d2d62f2f16e4cfe6e371e3c612932589e7
Kyle McDermed <kylemcdermed1@gmail.com>
2025-08-19 17:15:22 -0400

    learned cargo new, cargo build, cargo run, cargo check, cargo build --release

---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+C:\Users\kylem\projects\hello_world\hello_cargo\target\debug\hello_cargo.exe: C:\Users\kylem\projects\hello_world\hello_cargo\src\main.rs
---
+++ hello_cargo/target/release/hello_cargo.d
@@ -0,0 +1,1 @@
+C:\Users\kylem\projects\hello_world\hello_cargo\target\release\hello_cargo.exe: C:\Users\kylem\projects\hello_world\hello_cargo\src\main.rs
