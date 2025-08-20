96318e00237599aabf87f7a7f0ab51363533ce15
Kyle McDermed <kylemcdermed1@gmail.com>
2025-08-20 14:53:57 -0400

    handling the possible return values of comparing two numbers

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,31 +1,24 @@
 
 
-use std::io;  
-use rand::Rng; // Rng trait defines the method that the random number generator implements
+
+use std::cmp::Ordering;
+use std::io;
+use rand::Rng;
 
 
-fn main() { 
 
-     
-    println!("Guess the number!"); 
+fn main() {
 
-    // rand::thread_rng function gives us the particular random number generator we will use
-    // gen_range method takes a range expression as argument and generates a random number in range
-    // the range expression takes the form (start..=end) , inclusive on lower/upper bounds
-    // so we specify 1..=100 to request a number between 1 and 100
-    let secret_number = rand::rng().random_range(1..=100);
-
-    println!("The secret number is: {secret_number}");
-    println!("Please input your guess.");
-
-    let mut guess = String::new();
-    
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
-
-    
-    
+    // --snip--
     
     println!("You guessed: {guess}");
+
+    match(guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => println!("You win~"),
+    }
+
 }
+
+
