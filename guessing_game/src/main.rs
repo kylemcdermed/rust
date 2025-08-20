878f354f574339eda43d8147301be04374d52ec6
Kyle McDermed <kylemcdermed1@gmail.com>
2025-08-20 14:48:29 -0400

    adding documentation to the code

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,7 +1,7 @@
 
 
 use std::io;  
-use rand::Rng;
+use rand::Rng; // Rng trait defines the method that the random number generator implements
 
 
 fn main() { 
@@ -9,13 +9,16 @@ fn main() {
      
     println!("Guess the number!"); 
 
-
+    // rand::thread_rng function gives us the particular random number generator we will use
+    // gen_range method takes a range expression as argument and generates a random number in range
+    // the range expression takes the form (start..=end) , inclusive on lower/upper bounds
+    // so we specify 1..=100 to request a number between 1 and 100
     let secret_number = rand::thread_rng().gen_range(1..=100);
 
     println!("The secret number is: {secret_number}");
     println!("Please input your guess.");
 
-    let mut guess guess = String::new();
+    let mut guess = String::new();
     
     io::stdin()
         .read_line(&mut guess)
