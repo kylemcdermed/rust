049311cff3076d6726ece2903ac39520c7bcbbc4
Kyle McDermed <kylemcdermed1@gmail.com>
2025-08-22 16:49:27 -0400

    went through a whole lot to just get this to work.. patience pays, so does learning a new programming language

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,47 +1,37 @@
-
-
-
-use std::cmp::Ordering; // Ordering type is another enum with variants -- Less, Greater, Equal
+use std::cmp::Ordering; 
 use std::io;
 use rand::Rng;
 
-
-
 fn main() {
-
-    
     let secret_number = rand::rng().random_range(1..=100);
 
-    println!("The secret_number is: {secret_number}");
+    println!("The secret number is: {secret_number}");
 
     loop {
+        println!("Guess the number!");
 
-    println!("Guess the number!");
+        let mut guess = String::new();
 
-    let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+        // 👇 Correct syntax with match
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
 
-    let guess: u32 = guess.trim().parse() {
-        Ok(num) => num,
-        Err(_) => continue,
-    };
+        println!("You guessed: {guess}");
 
-
-    println!("You guessed: {guess}");
-
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => { 
-            println!("You win!"),
-            break;
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break; // exit loop when correct
+            }
         }
     }
-
-    }
-
 }
 
