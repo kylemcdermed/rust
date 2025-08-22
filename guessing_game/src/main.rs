131dd42815fe3010047c7e7a953ab972359cbd5e
Kyle McDermed <kylemcdermed1@gmail.com>
2025-08-22 16:20:36 -0400

    fixed the main.rs code a bit to convert a string to a interger

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -10,27 +10,24 @@ use rand::Rng;
 fn main() {
 
     // --snip--
+
     
+    let mut guess = String::new()
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line")
+
+    let guess: u32 = guess.trim().parse().expect("Please type a number!")
+
+
     println!("You guessed: {guess}");
 
-    // cmp method compares two values and can be called on anything being compared
-    // here its comparing guess to secret number
-    // then it returns a variant of the Ordering enum we brought into scope with the use statement
-    // we use match expression to decide what to do next based on which variant of Ordering was
-    // returns from the call to cmp with values in guess and secret_number
-    // match expression is made up of arms, an arm consists of a pattern to match against
-    // Rust takes the value guven to match and looks through each arms pattern in turn
     match(guess.cmp(&secret_number) {
         Ordering::Less => println!("Too small!"),
         Ordering::Greater => println!("Too big!"),
         Ordering::Equal => println!("You win~"),
     }
-    // lets say the user guessed 50 and randomly generated secret_number 38
-    // when match compares guess onto secret_number, it will returns Ordering::Greater because 50
-    // is greater than 38
-    // the code checks each arms pattern which arms matches the expression and will execute and
-    // print, in this case "Too big!"
 
 }
-
 
