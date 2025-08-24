2216a4d5742ae32179760b1dd1870e873bc138aa
Kyle McDermed <kylemcdermed1@gmail.com>
2025-08-24 12:16:06 -0400

    note taking on section variables and mutability

---
+++ variables_and_mutability.rs
@@ -0,0 +1,11 @@
+// Variables and Mutability
+//
+//
+// Variables are immuteable. 
+// This is an advantage Rust gives you for the safety of easy concurrency.
+// You still however have an option to make your variables mutable.
+//
+//
+// When a variable is immutable, once a value is bound to a name, you cant change that value.
+// To illustrate this, generate a new project called `variables` in your projects dir by using
+// `cargo new variables`:
