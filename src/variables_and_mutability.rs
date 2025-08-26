2529ef083806c22d98465864f01768b9c1107fcd
Kyle McDermed <kylemcdermed1@gmail.com>
2025-08-26 18:26:55 -0400

    rust notes on const

---
+++ src/variables_and_mutability.rs
@@ -0,0 +1,14 @@
+// Constants are variables that are bond to a name and are not allowed to change 
+//
+// few difference between constants and variables 
+//
+// you are allowed to use mut with constants 
+// consts arent just immutable by default -- theyre always immutable 
+//
+// here is a example of const declaration
+// const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
+//
+// Rust has a naming convention for constants to use all UPPERCASE with _UNDER_SCORES_ in between
+// words 
+//
+// 
