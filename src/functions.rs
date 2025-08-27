bc20d4982a6eb94ee25f5a5eada429757ee18aa3
Kyle McDermed <kylemcdermed1@gmail.com>
2025-08-27 19:20:44 -0400

    working with creating new functions

---
+++ src/functions.rs
@@ -0,0 +1,9 @@
+// FUNCTIONS 
+//
+// the most important function in the Rust language is the `main` function, which is the entry
+// point of many programs
+//
+// youve aso seen the `fn` keyword, which allows you to declare a new function
+//
+// Rust uses snake_case as the convential style for function and variable names, all letters are
+// lower case and underscores separate words
--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,11 @@
 
 
-use std::io
 
+fn main(){
+    println!("Hello, World!");
+    another_function();
+}
 
-fn main() {
-    let a = [1,2,3,4,5];
-
-    println!("Please enter an array index.");
-
-    let mut index = String::new();
-
-    io::stdin()
-        .read_line(&mut index)
-        .expect("Failed to read line");
-
-    let index: usize = index
-        .trim()
-        .parse()
-        .expect("Index entered was not a number");
-
-    let element = a[index];
-
-    println!("The value of the element at index {index} is: {element}");
+fn another_function() {
+    println!("Another function.");
 }
