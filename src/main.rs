42a688666868aa2bb7d464f458e0916edc48b21d
Kyle McDermed <kylemcdermed1@gmail.com>
2025-08-27 19:33:51 -0400

    defining multiple parameters, separate the parameter declarations with commas

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
 
 
 
-fn main(){
-    println!("Hello, World!");
-    another_function(5);
+
+
+fn main() {
+    print_labeled_measurement(5,'h');
 }
 
-fn another_function(x: i32) {
-    println!("The value of x is {x}");
+fn print_labeled_measurement(value: i32, unit_label: char) {
+    println!("The measurement is: {value} {unit_label}");
 }
