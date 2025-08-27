20080e1d92eaa286dc80305facfe7eb6fc732dc6
Kyle McDermed <kylemcdermed1@gmail.com>
2025-08-27 19:23:45 -0400

    adding notes to function notes

--- src/functions.rs
+++ src/functions.rs
@@ -7,3 +7,18 @@
 //
 // Rust uses snake_case as the convential style for function and variable names, all letters are
 // lower case and underscores separate words
+//
+// we define a function in Rust by entering the fn followed by a function name and a set of
+// parenthese. the curly brackets tell the compiler where the function body begins and ends
+//
+// we can call any function weve defined by entering its name followed by a set of parentheses.
+//
+// because `another_function` is defined in the program, it can be called from inside the `main`
+// function.
+//
+// note that we defined `another_function` after the main function in the source code;
+//
+// we could have defined it before as well, Rust doesnt care where you define yours functions, only
+// that theyre defined somewhere in a scope that can be seen by th caller
+//
+// were going to create a new binary project called functions to explore further
