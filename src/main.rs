c58aebf498ce0cabc679924542aa89372bc64332
Kyle McDermed <kylemcdermed1@gmail.com>
2025-08-27 12:18:48 -0400

    example of floating point numbers

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,8 @@
 
 
 
+
 fn main() {
-    let x = 5;
-    let x = x + 1;
-    {
-        let x = x * 2;
-        println!("The value os x in the inner scope is: {x}");
-    }
-    println!("The value of x is: {x}")
+    let x = 2.0; // f64 
+    let y: f32 = 3.0; // f32
 }
