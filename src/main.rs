621449fe18cba83c6d81c115894d349c2f702dc2
Kyle McDermed <kylemcdermed1@gmail.com>
2025-08-27 20:02:58 -0400

    funky, crazy looking return on this function

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
 
 
 
+fn five() -> i32 {
+    5
+}
+
 fn main() {
-    let y = {
-        let x = 3;
-        x + 1
-    };
-    println!("The value of y is: {y}");
+    let x = five();
+    println!("The value of x is: {x}");
 }
