64d102ce1887c55e092be2390fabc69a3367b322
Kyle McDermed <kylemcdermed1@gmail.com>
2025-08-30 10:50:30 -0400

    control flow notes

--- src/control_flow.rs
+++ src/control_flow.rs
@@ -1,3 +1,11 @@
 // CONTROL FLOW
 //
+// the most common constructs that let you control the flow of execution of Rust code are if
+// expressions and loops
 //
+//
+// IF EXPRESSIONS
+//
+// an if expression allows you to branch your code depending on conditions, you provide a condition
+// and then state, if this condition is met, run this block of code, if the condition is not met,
+// do not run this block of code
