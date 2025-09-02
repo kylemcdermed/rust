ad7dd997a05922c24433907c9924e5d3bcbb378f
Kyle McDermed <kylemcdermed1@gmail.com>
2025-09-02 11:45:50 -0400

    more ownership notations

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,8 @@
 
 
 fn main() {
-
-    let s = "hello";
-
-}
+    // s is not evalid here, since its not declared yet
+    let s = "hello"; // s is valud from this point forward
+    // do stuff with s
+} // this scope is now over, and s is no longer valid
 
