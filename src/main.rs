4c70f1adb6800289df31d25ce38e6d811da95b19
Kyle McDermed <kylemcdermed1@gmail.com>
2025-09-08 09:37:20 -0400

    code

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,23 @@
 
 
 
-fn first_word(s: &String) -> &str {
-    let bytes = s.as_bytes();
-
-    for (i, &item) in bytes.iter().enumerate() {
-        if item == b' ' {
-            return &s[0..1];
-        }
-    }
-
-    &s[..]
-}
-
 fn main() {
-    let mut s = String::from("hello");
+    let my_string = String::from("hello world");
 
-    let word = first_word(&s);
+    // first_word function works on slices of Strings, whether partial or whole
+    let word = first_word(&my_string[0..6]);
+    let word = first_word(&my_string[..]);
+    // first_word also works on references to Strings which are equivalent 
+    // to the whole slices of Strings
+    let word = first_word(&my_string);
 
-    s.clear(); // error!
-    
-    println!("the first word is: {word}");
+    let my_string_literal = "hello world";
+
+    //  first_word works on slices of string literals, whether partial or whole 
+    let word = first_word(&my_string_literal[0..6]);
+    let word = first_word(&my_string_literal[..]);
+
+    // because string literals *are* string slices already, this works too, without any slice
+    // syntax!
+    let word = first_word(my_string_literal);
 }
