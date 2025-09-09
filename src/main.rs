aa853cf06355b82c5301893a50696e3829092244
Kyle McDermed <kylemcdermed1@gmail.com>
2025-09-09 09:09:22 -0400

    code for struct

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,13 @@
 
 
 
+
+
 fn main() {
-    let my_string = String::from("hello world");
-
-    // first_word function works on slices of Strings, whether partial or whole
-    let word = first_word(&my_string[0..6]);
-    let word = first_word(&my_string[..]);
-    // first_word also works on references to Strings which are equivalent 
-    // to the whole slices of Strings
-    let word = first_word(&my_string);
-
-    let my_string_literal = "hello world";
-
-    //  first_word works on slices of string literals, whether partial or whole 
-    let word = first_word(&my_string_literal[0..6]);
-    let word = first_word(&my_string_literal[..]);
-
-    // because string literals *are* string slices already, this works too, without any slice
-    // syntax!
-    let word = first_word(my_string_literal);
+    let user1 = User {
+        active: bool,
+        username: String::from("someusername123"),
+        email: String::from("someone@example.com"),
+        sing_in_count: 1,
+    };
 }
