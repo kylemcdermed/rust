289c016861e1049ac50ce6a67a5a1b0f622d4ead
Kyle McDermed <kylemcdermed1@gmail.com>
2025-09-09 09:17:44 -0400

    adding notes before field init shorthand

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,17 @@
 
 
 
-
+fn build_user(email: String, username: String) -> User {
+    User {
+        active: true,
+        username: username,
+        email: email,
+        sign_in_count: 1,
+    }
+}
 
 fn main() {
-    let user1 = User {
+    let mut user1 = User {
         active: bool,
         username: String::from("someusername123"),
         email: String::from("someone@example.com"),
--- src/structs.rs
+++ src/structs.rs
@@ -41,4 +41,22 @@
 // if the instance is mutable, we can change a value by using the dot notation and assigning into a
 // particular field
 //
+// note that the entire instance is mutable; Rust doesnt allow us to mark only certain fields as
+// mutable, as with any expression, we can construct a new instance of the struct as the last
+// expression in the function body to implicitly return that new instance
+//
+// lets create a build_user function that returns a User instance with the given email and
+// username. the active field gets the value of true and the sing_in_count gets a value of 1
+//
+// it makes sense to name the function parameters with the same name as the struct fields but
+// having to repeat the email an dusername field names and variables is a bit tedious. if the
+// struct had more fields repeating each name would get annoying luckily theres a shorthand
+//
+//
+// USING THE FIELD INIT SHORTHAND 
+//
+// because the paraneter names and struct fields names are exactly the same, we can use the field
+// init shorthand syntax to rewrite build_user so it behaves exactly like the same but doesnt have
+// the repitition of username and email
+//
 //
