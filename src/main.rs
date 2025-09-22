476cc9d2d913e8721877cb37c89aea9e124b45aa
Kyle McDermed <kylemcdermed1@gmail.com>
2025-09-22 09:13:56 -0400

    Adding code for enum

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,21 @@
-#[derive(Debug)]
-struct Rectangle {
-    width: u32,
-    height: u32,
-}
-impl Rectangle {
-    fn square(size: u32) -> Self {
-        Self {
-            width: size,
-            height: size.
-        }
-    }
-    fn area(&self) -> u32 {
-        self.width * self.height
-    }
-    fn width(&self) -> bool {
-        self.width > 0
-    }
-    fn can_hold(&self, other: &Rectangle) -> bool {
-        self.width > other.width && self.height > other.height
-    }
+
+
+enum IpAddrKind {
+    V4, 
+    V6,
+};
+
+struct IpAddr {
+    kind: IpAddrKind,
+    address: String,
 }
 
-fn main() {
-    let rect1 = Rectangle {
-        width:30,
-        height: 50,
-    };
-    let rect2 = Rectangle {
-        width: 10, 
-        height: 40,
-    };
-    let rect3 = Rectangle {
-        width: 60, 
-        height: 45,
-    };
-    println!("Can rect1 hold rect2 {}", rect1.can_hold(&rect2));
-    println!("Can rect1 hold rect3 {}", rect1.can_hold(&rect3));
-}
+let home = IpAddr {
+    kind: IpAddrKind::V4,
+    address: String::from("127.0.0.1"),
+};
+
+let loopback = IpAddr {
+    kind: IpAddrKind::V6,
+    address: String::from("::1");
+};
