d97c7aad3e99bd9c6b4789889f53ba562fa4ac9d
Kyle McDermed <kylemcdermed1@gmail.com>
2025-09-22 09:18:46 -0400

    code on enum

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,14 @@ enum IpAddrKind {
     V6,
 };
 
+enum IpAddr {
+    V4(String),
+    V6(String),
+}
+
+let home = IpAddr::V4(String::from("127.0.0.1"));
+let loopback = IpAddr::V6(String::from("::01"));
+
 struct IpAddr {
     kind: IpAddrKind,
     address: String,
