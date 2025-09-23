24ba0bba4814dce1a56c2c61fa317c2c53b6fb05
Kyle McDermed <kylemcdermed1@gmail.com>
2025-09-23 11:20:17 -0400

    doll roll example

--- src/main.rs
+++ src/main.rs
@@ -50,12 +50,25 @@ fn plus_one(x: Option<i32>) -> Option<i32> {
         Some(i) => Some(i+1),
     }
 }
-
-
+/*
 fn main() {
     let five = Some(5);
     let six = plus_one(five);
     let none = plus_one(None);
+}
+*/
+
+fn add_fancy_hat() {}
+fn remove_fancy_hat() {}
+fn move_player(num_spaces: u8) {}
+
+fn main() {
+    let dice_roll = 9;
+    match dice_roll {
+        3 => add_fancy_hat(),
+        7 => remove_fancy_hat(),
+        other => move_player(other),
+    }
 }
 
 
