f704e1281597ff35d173141856b29631e16ac881
Kyle McDermed <kylemcdermed1@gmail.com>
2025-09-24 16:14:10 -0400

    adding more code

--- src/main.rs
+++ src/main.rs
@@ -15,10 +15,21 @@ if let Some(max) = config_max {
 }
 */
 
+/*
 // using else with if let 
 let mut count = 0;
 match coin {
     Coin::Quarter(state) => println!("State quarter from {state:?}"),
     _ => count += 1,
 }
+*/
+
+// using if let and else expression like this 
+let mut count = 0;
+if let Coin::Quarter(state) = coin {
+    println!("State quarter from {state:?}");
+} else {
+    count += 1
+}
+
 
