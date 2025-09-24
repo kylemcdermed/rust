ded217fda1e4dcefa08f023b00a5e4b08ea8c1b6
Kyle McDermed <kylemcdermed1@gmail.com>
2025-09-24 16:05:36 -0400

    adding the if let code

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,15 @@
 
-
+/*
 let config_max = Some(3u8);
 match config_max {
     Some(max) => println!("The maximum is configured to be {max}"),
     _ => (),
 }
+*/
 
+// using if let  
+let config_max = Some(3u8);
+if let Some(max) = config_max {
+    println!("The maximum is configured to be {max}");
+}
 
