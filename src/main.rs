be72c71c84f6b7fd08f7a7b77f66640899a7fa03
Kyle McDermed <kylemcdermed1@gmail.com>
2025-09-24 16:11:01 -0400

    adding more code

--- src/main.rs
+++ src/main.rs
@@ -7,9 +7,18 @@ match config_max {
 }
 */
 
+/*
 // using if let  
 let config_max = Some(3u8);
 if let Some(max) = config_max {
     println!("The maximum is configured to be {max}");
+}
+*/
+
+// using else with if let 
+let mut count = 0;
+match coin {
+    Coin::Quarter(state) => println!("State quarter from {state:?}"),
+    _ => count += 1,
 }
 
